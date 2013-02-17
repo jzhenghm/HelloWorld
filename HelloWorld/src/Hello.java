ed63ec9699bd6542213495184751c71bcb2b99cf
jzhenghm <jzheng@hotmail.com>
2013-02-17 15:26:15 -0500

    Signed-off-by: jzhenghm <jzheng@hotmail.com>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -2,7 +2,11 @@
 public class Hello {
 	
 	public static void main(String[] args){
-		System.out.println("hello world");
+		
+		System.out.println("hello world!");
+		
+		System.out.println("another call: hello world!");
+	
 	}
 	
 
